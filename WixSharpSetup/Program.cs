7c5ed2f98e37af256adbb8185f595c4c0604c5a3
Giles Bathgate <giles.bathgate@gmail.com>
2019-08-27 14:28:12 +0100

    Initial files.

---
+++ WixSharpSetup/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using WixSharp;
+
+// DON'T FORGET to update NuGet package "WixSharp".
+// NuGet console: Update-Package WixSharp
+// NuGet Manager UI: updates tab
+
+namespace WixSharpSetup
+{
+    class Program
+    {
+        static void Main()
+        {
+            var project = new ManagedProject("MyProduct",
+                             new Dir(@"%ProgramFiles%\My Company\My Product",
+                                 new File("Program.cs")));
+
+            project.GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b");
+
+            //custom set of standard UI dialogs
+            project.ManagedUI = new ManagedUI();
+
+            project.ManagedUI = ManagedUI.Default;
+
+            project.OutDir = @"..\";
+
+            var ahHa = @"..\ExampleConsoleApp\bin\Debug"; // I had forgotten that currently this directory
+            //has to be hard coded but actually it could be inferred by parsing the .csproj
+
+            var harvester = new Harvester(project, ahHa, "INSTALLDIR");
+            harvester.AddProject(new ProjectReference(@"..\ExampleConsoleApp\ExampleConsoleApp.csproj"));
+
+            project.BuildMsi();
+        }
+
+    }
+}
---
+++ WixSharpSetup/ProjectReference.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace WixSharp
+{
+    public class ProjectReference
+    {
+        public string Name { get; }
+
+        public string SourceDir { get; set; }
+
+        public string TargetDir { get; set; }
+
+        public string ProjectPath { get; }
+
+        public bool Binaries { get; set; } = true;
+
+        public bool Content { get; set; } = true;
+
+        public bool Satellites { get; set; } = true;
+
+
+        public ProjectReference(string projectPath)
+        {
+            ProjectPath = projectPath;
+            Name = Path.GetFileNameWithoutExtension(projectPath);
+        }
+    }
+}
